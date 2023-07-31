3bfac5f87949f9c2649a8bd790e32f1f386c06cd
Caleb Archer <calebandcorey@gmail.com>
2023-07-31 22:28:43 +1200

    Check for existence of environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+# Check if SLAVE_SECRET is set
+if [ -z "$SLAVE_SECRET" ]; then
+    echo "SLAVE_SECRET environment variable is not set."
+    exit 1
+fi
+
+# Check if MASTER_URL is set
+if [ -z "$MASTER_URL" ]; then
+    echo "MASTER_URL environment variable is not set."
+    exit 1
+fi
+
+# Check if SLAVE_NAME is set
+if [ -z "$SLAVE_NAME" ]; then
+    echo "SLAVE_NAME environment variable is not set."
+    exit 1
+fi
+
 echo "Loading secret..."
 echo $SLAVE_SECRET > /var/smokeping/secret.txt
 
@@ -8,6 +26,6 @@ chown smokeping:root /var/smokeping/secret.txt
 chown smokeping:root /usr/cache/smokeping
 
 echo "Starting smokeping..."
-smokeping --master-url=$MASTER_URL --cache-dir=/usr/cache/smokeping --shared-secret=/var/smokeping/secret.txt --slave-name=archer
+smokeping --master-url=$MASTER_URL --cache-dir=/usr/cache/smokeping --shared-secret=/var/smokeping/secret.txt --slave-name=$SLAVE_NAME
 
 sleep infinity
