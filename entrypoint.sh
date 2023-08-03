9b3999151e14450a4057ae5ed04819d228de40b2
Caleb Archer <calebandcorey@gmail.com>
2023-08-03 18:04:42 +1200

    Add exit status check and documentation to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,11 +21,20 @@ fi
 echo "Loading secret..."
 echo $SLAVE_SECRET > /var/smokeping/secret.txt
 
+# Make sure the secret and cache are available to Smokeping
 chmod 600 /var/smokeping/secret.txt
 chown smokeping:root /var/smokeping/secret.txt
 chown smokeping:root /usr/cache/smokeping
 
-echo "Starting smokeping..."
+echo "Starting Smokeping slave..."
 smokeping --master-url=$MASTER_URL --cache-dir=/usr/cache/smokeping --shared-secret=/var/smokeping/secret.txt --slave-name=$SLAVE_NAME
 
+# Check the exit status of Smokeping
+if [ $? -ne 0 ]; then
+  echo "Failed to start Smokeping slave. Exiting."
+  exit 1
+fi
+
+# The Smokeping daemon was successful and has exited after starting Smokeping probe processes.
+# Suspend this process infinitely so the container doesn't exit
 sleep infinity
